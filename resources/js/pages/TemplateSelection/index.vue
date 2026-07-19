d32434adf1b5a08c270c16dba3c744ef167f725b
siobhan-mei <siobhanmeic@gmail.com>
2026-07-19 20:33:18 +0800

    Add a new page: Template Selection

---
+++ resources/js/pages/TemplateSelection/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="main-bg" style="flex-direction: column">
+        <SiteCredit>
+            <template #start>
+                <router-link
+                    to="/"
+                    class="btn white-button"
+                    aria-label="Back to home"
+                    @click="playClickSound"
+                >
+                    <BackButton />
+                </router-link>
+                <div
+                    class="card secondary-logo-card"
+                >
+                    <img :src="logo" class="logo-size" alt="oceanbooth logo" />
+                </div>
+                <p class="logo-title">OceanBooth</p>
+
+            </template>
+        </SiteCredit>
+    </div>
+</template>
+
+<script>
+import SiteCredit from "@components/SiteCredit.vue";
+import BackButton from "@components/svgs/BackButton.vue";
+import logo from "@assets/images/logo.svg";
+import clickSfx from "@assets/sfx/click.mp3";
+import { useSound } from "@composables/useSound";
+
+export default {
+    name: "TemplateSelection",
+    components: {
+        SiteCredit,
+        BackButton,
+    },
+    setup() {
+        const { play: playClickSound } = useSound(clickSfx);
+        return { playClickSound, logo };
+    },
+};
+</script>
+
+<style scoped>
+.logo-size {
+    width: 1.4rem;
+    height: 1.4rem;
+}
+
+</style>
