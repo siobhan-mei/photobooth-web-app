ec2b6a847ca8d8a18be7fae24336ca337c40c1d4
siobhan-mei <siobhanmeic@gmail.com>
2026-07-19 20:17:44 +0800

    Refactor polaroid decors as standalone vue components

---
+++ resources/js/components/FloatingPolaroids.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="absolute-bg-container">
+        <img
+            v-for="(item, index) in images"
+            :key="index"
+            :src="item.src"
+            class="float"
+            :style="{
+                width: item.width + 'px',
+                marginLeft: item.marginLeft
+                    ? item.marginLeft + 'px'
+                    : undefined,
+                marginBottom: item.marginBottom
+                    ? item.marginBottom + 'px'
+                    : undefined,
+                animationDelay: index * 0.5 + 's',
+            }"
+            alt="polaroid"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FloatingPolaroids",
+    props: {
+        images: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.absolute-bg-container {
+    position: absolute;
+    bottom: 0;
+    z-index: 0;
+}
+.float {
+    animation: float 3s ease-in-out infinite;
+}
+@keyframes float {
+    0%,
+    100% {
+        transform: translateY(0px);
+    }
+    50% {
+        transform: translateY(-15px);
+    }
+}
+</style>
--- resources/js/pages/HomePage/index.vue
+++ resources/js/pages/HomePage/index.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="main-bg" style="flex-direction: column">
-        <HeroSection />
+        <SiteCredit />
         <div class="d-flex flex-column align-items-center">
             <div class="d-flex flex-row" style="margin-top: 30%">
                 <div
-                    class="card logo-card p-2 text-center align-items-center justify-content-center"
+                    class="card main-logo-card p-2 text-center align-items-center justify-content-center"
                 >
-                    <img :src="logo" class="logo-size" alt="oceanbooth logo" />
+                    <img :src="logo" class="logo-size width: 1rem; height: 1rem;" alt="oceanbooth logo" />
                 </div>
-                <h2>OceanBooth</h2>
+                <h2 class="logo-title">OceanBooth</h2>
             </div>
             <p class="description text-center">
                 Tap Start to ride the ultimate vibe wave and turn your<br />
                 photos into awesome retro polaroids instantly without<br />
                 getting annoying sand in your shoes!
             </p>
-            <button class="btn pink-button" href="#" role="button">
+            <router-link 
+                to="/template-selection"
+                class="btn pink-button"
+                @click="playClickSound"
+            >
                 Start Session
-            </button>
+            </router-link>
             <div class="polaroid-card card p-3 text-left">
                 Recent Polaroids
                 <p class="polaroid-card-placeholder text-center">
@@ -26,45 +30,51 @@
             </div>
         </div>
         <img :src="homeBg" class="home-bg" alt="wave background" />
-        <div class="absolute-bg-container">
-            <img :src="homeDecor1" class="home-decor-1 float" alt="polaroid" style="width: 370px;" />
-            <img :src="homeDecor2" class="home-decor-2 float" alt="polaroid" style="width: 370px; margin-left: -130px; margin-bottom: -220px" />
-            <img :src="homeDecor3" class="home-decor-3 float" alt="polaroid" style="width: 370px; margin-left: -40px;" />
-        </div>
+            <FloatingPolaroids :images="imageData"/>
     </div>
 </template>
 
 <script>
-import HeroSection from "@components/HeroSection.vue"
-import NullIndicator from "@components/svgs/NullIndicator.vue"
+import SiteCredit from "@components/SiteCredit.vue";
+import NullIndicator from "@components/svgs/NullIndicator.vue";
+import FloatingPolaroids from "@components/FloatingPolaroids.vue";
 import logo from "@assets/images/logo.svg";
 import homeBg from "@assets/images/home-bg.svg";
 import homeDecor1 from "@assets/images/home-decor-1.svg";
 import homeDecor2 from "@assets/images/home-decor-2.svg";
 import homeDecor3 from "@assets/images/home-decor-3.svg";
+import clickSfx from "@assets/sfx/click.mp3";
+import { useSound } from "@composables/useSound";
 
 export default {
     name: "HomePage",
     components: {
-        HeroSection,
+        SiteCredit,
         NullIndicator,
+        FloatingPolaroids,
+    },
+    setup() {
+        const { play: playClickSound } = useSound(clickSfx);
+        return { playClickSound };
     },
     data() {
         return {
             logo,
             homeBg,
-            homeDecor1,
-            homeDecor2,
-            homeDecor3
+            imageData: [
+                { src: homeDecor1, width: 370 },
+                { src: homeDecor2, width: 370, marginLeft: -130, marginBottom: -220 },
+                { src: homeDecor3, width: 370, marginLeft: -40 },
+            ],
         };
     },
 };
 </script>
 
 <style scoped>
-h2 {
-    font-family: "Cardo", serif;
-    font-weight: 700;
+.logo-size {
+    width: 1.8rem;
+    height: 1.8rem;
 }
 .description {
     margin-top: 4%;
@@ -93,25 +103,5 @@ h2 {
     display: block;
     margin-top: auto;
 }
-.absolute-bg-container {
-    position: absolute;  
-    bottom: 0;
-    z-index: 0;
-}
-.float {
-    animation: float 3s ease-in-out infinite;
-}
-@keyframes float {
-    0%, 100% { transform: translateY(0px); }
-    50% { transform: translateY(-15px)}
-}
-.float:nth-child(1) {
-    animation-delay: 0s;
-}
-.float:nth-child(2) {
-    animation-delay: 0.5s;
-}
-.float:nth-child(3) {
-    animation-delay: 1s;
-}
+
 </style>
