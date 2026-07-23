9359de61278699ef6fd83b261844f32c84a0380c
siobhan-mei <siobhanmeic@gmail.com>
2026-07-23 22:48:02 +0800

    Add new page: Photo Editor page

---
+++ resources/js/pages/PhotoEditor/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-bg">
+        <TopHeader variant="page" @back-click="playClickSound" />
+    </div>
+</template>
+<script>
+import TopHeader from "@components/TopHeader.vue";
+
+export default {
+    name: "PhotoEditor",
+    components: {
+        TopHeader,
+    },
+    setup() {
+        const { play: playClickSound } = useSound(clickSfx, 0.4);
+        return { playClickSound };
+    },
+}
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@pages/HomePage/index.vue';
 import TemplateSelection from '@pages/TemplateSelection/index.vue';
+import PhotoEditor from '@pages/PhotoEditor/index.vue';
 
 const routes = [
     { path: '/', name: 'home', component: HomePage },
     { path: '/template-selection', name: 'templateSelection', component: TemplateSelection },
+    { path: '/photo-editor', name: 'photoEditor', component: PhotoEditor },
 ];
 
 export default createRouter({
